b363213a474d48fbe0980d9d124d71c894600bd9
Pranav <pranavkp.me@outlook.com>
2023-05-09 15:06:22 +0530

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), cssInjectedByJsPlugin()],
   build: {
-    lib: {
-      entry: resolve(__dirname, 'lib/main.js'),
-      name: 'chat-bot-ui',
-      // the proper extensions will be added
-      fileName: 'chat-bot-ui',
-    },
+    outDir: 'dist',
     cssCodeSplit: false,
     rollupOptions: {
       input: {
         app: './src/main.tsx'
+      },
+      output: {
+        entryFileNames: 'main.js',
+        chunkFileNames: '[name]-[hash].js',
+        assetFileNames: '[name]-[hash][extname]'
       }
     }
   }

f895d64a8717c479cbba73d2a96d900ef355f557
Pranav <pranavkp.me@outlook.com>
2023-05-09 15:08:21 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,8 +11,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         app: './src/main.tsx'
-      },
-
+      }
     }
   }
 })

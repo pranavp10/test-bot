3aae3f1638262cbe47bae1b9fb3f9f9e8be1c0cc
Pranav <pranavkp.me@outlook.com>
2023-05-10 13:55:59 +0530

    bot integration added

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://yfnpraftrjzpljagrzji.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmbnByYWZ0cmp6cGxqYWdyemppIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMxODQwMDgsImV4cCI6MTk5ODc2MDAwOH0.u1gViHf4swTg8ftqIC8zcSmKFF8Utx9OSMYqZfeL6Aw';
 
 if (!SUPABASE_URL) throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL');
 if (!SUPABASE_ANON_KEY) throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY');
